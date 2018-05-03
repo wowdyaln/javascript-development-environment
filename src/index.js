e5e375b5059652975a34df5e833ed5021e534c2f
wowdyaln <rogersmk@gmail.com.tw>
2018-05-03 14:17:02 +0800

    96 manipulating data via JSON Server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import {getUsers} from './api/userApi.js'
+import { getUsers, deleteUser} from './api/userApi.js'
 
 // populate table of users via API call.
 getUsers().then(result => {
@@ -15,6 +15,20 @@ getUsers().then(result => {
   })
 
   global.document.getElementById('users').innerHTML = usersBody
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser')
+
+  // must are array.from to create a real array from a DOM collection
+  // getElementByClassName only return an 'array like' object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event){
+      const element = event.target
+      event.preventDefault()
+      deleteUser(element.attributes["data-id"].value)
+      const row = element.parentNode.parentNode
+      row.parentNode.removeChild(row)
+    }
+  })
 })
 
 
