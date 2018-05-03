829fc8026693237168f4f3dba845a9ef6f4a95b1
wowdyaln <rogersmk@gmail.com.tw>
2018-05-03 13:15:18 +0800

    95 serving mock data via JSON Server

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ getUsers().then(result => {
 
   result.forEach(user => {
     usersBody += `<tr>
-      <td><a herf="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
       <td>${user.id}</td>
       <td>${user.firstName}</td>
       <td>${user.lastName}</td>
